60999febfc8d1c7f349e8dc6a81a6a4fdefb8de7
SynthiaAtieno <ondengsynthia222@gmail.com>
2023-11-22 11:58:57 +0300

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce/constants/theme.dart';
 import 'package:e_commerce/screens/welcome/welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
